31291668a464550476087d79d6dcad3efe769497
Sunny Yang <sunnyadn@foxmail.com>
2020-10-27 19:41:15 +0800

    Api for get members

---
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/MemberResource.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.capability.gtb.entrancequiz.controller;
+
+import com.thoughtworks.capability.gtb.entrancequiz.model.Member;
+import com.thoughtworks.capability.gtb.entrancequiz.service.MemberService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("members")
+public class MemberResource {
+
+    private final MemberService memberService = new MemberService();
+
+    @GetMapping
+    public List<Member> getMembers() {
+        return memberService.getMembers();
+    }
+}
---
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/MemberService.java
@@ -0,0 +1,40 @@
+package com.thoughtworks.capability.gtb.entrancequiz.service;
+
+import com.thoughtworks.capability.gtb.entrancequiz.model.Member;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberService {
+    private int currentIndex = 1;
+    private final List<Member> members = new ArrayList<>();
+
+
+    public MemberService() {
+        addMember("成吉思汗");
+        addMember("鲁班七号");
+        addMember("太乙真人");
+        addMember("钟无艳");
+        addMember("花木兰");
+        addMember("雅典娜");
+        addMember("芈月");
+        addMember("白起");
+        addMember("刘禅");
+        addMember("庄周");
+        addMember("马超");
+        addMember("刘备");
+        addMember("哪吒");
+        addMember("大乔");
+        addMember("蔡文姬");
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void addMember(String name) {
+        Member member = new Member(currentIndex, name);
+        members.add(member);
+        currentIndex++;
+    }
+}
