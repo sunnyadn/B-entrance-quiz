9a7cf2f810ae6bf0d882f2592c8635f7224e7814
Sunny Yang <sunnyadn@foxmail.com>
2020-10-27 20:21:00 +0800

    enable cors

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
@@ -2,9 +2,22 @@ package com.thoughtworks.capability.gtb.entrancequiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GtbEntranceQuizApplication {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/members").allowedOrigins("http://localhost:1234");
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GtbEntranceQuizApplication.class, args);
